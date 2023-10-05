5a35fa44c61abc63d526dae98d0f8fc46f61dcad
Ayanga Oluwamurewa <phoenixflower2005@gmail.com>
2023-10-05 19:55:46 +0100

    completed mini dictionary app

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template, url_for, request
-import datetime, json
+from flask import Flask, render_template, url_for, request, flash, current_app
+import datetime, json, pymysql, os
 from flaskext.mysql import MySQL
-import pymysql
 
 app = Flask(__name__)
 
+app.secret_key = 'secret'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 app.config['MYSQL_DATABASE_DB'] = 'dictionary'
 app.config['MYSQL_DATABASE_USER'] = 'root'
@@ -18,16 +18,19 @@ def index():
     user_input = ''
     if request.method == 'POST':
         user_input = request.form['word']
-        conn = mysql.get_db()
-        cursor = conn.cursor()
-        cursor.execute('select meaning from dictionary.word where word=%s', (user_input))
-        result = cursor.fetchall()
-        conn.commit()
-        cursor.close()
-        if (len(result) > 0):
-            user_response = result[0]['meaning']
+        if user_input == '':
+            flash('Please input a word!', 'flash-error')
         else:
-            user_response = 'This word does not exist in this dictionary, try another word'
+            conn = mysql.get_db()
+            cursor = conn.cursor()
+            cursor.execute('select meaning from dictionary.word where word=%s', (user_input))
+            result = cursor.fetchall()
+            conn.commit()
+            cursor.close()
+            if (len(result) > 0):
+                user_response = result[0]['meaning']
+            else:
+                user_response = 'This word does not exist in this dictionary, try another word'
         
     return render_template("index.html", user_response = user_response, user_input = user_input)
 
@@ -39,6 +42,7 @@ def dashboard():
     result = cursor.fetchall()
     conn.commit()
     cursor.close()
+
     return render_template("Dashboard.html", entries = result)
 
 @app.route('/word', methods=['POST'])
@@ -46,13 +50,30 @@ def add_word():
     req = request.get_json()
     word = req['word']
     meaning = req['meaning']
-    conn = mysql.get_db()
-    cursor = conn.cursor()
-    cursor.execute('insert into word(word, meaning) values(%s, %s)', (word, meaning))
-    conn.commit()
-    cursor.close()
+    if word == '' or meaning == '':
+        flash('Please fill out all fields!', 'flash-error')
+    else:
+        conn = mysql.get_db()
+        cursor = conn.cursor()
+        cursor.execute('insert into word(word, meaning) values(%s, %s)', (word, meaning))
+        conn.commit()
+        cursor.close()
+        flash('Word succesfully added!', 'flash-success')
 
     return json.dumps('success')
+
+@app.route('/add_logo', methods=['POST'])
+def add_logo():
+    image = request.files['file']
+
+    if image:
+        filepath = os.path.join( current_app.root_path, 'static/images/logo.png')
+        image.save(filepath)
+        flash('Success!', 'flash-success')
+    else:
+        flash('Error!', 'flash-error')
+
+    return 'success'
 
 @app.route('/word/<id>/delete', methods=['POST'])
 def delete_word(id):
@@ -62,6 +83,25 @@ def delete_word(id):
     cursor.execute('delete from word where id=%s', (word_id))
     conn.commit()
     cursor.close()
+    flash('Word succesffuly deleted!', 'flash-success')
+
+    return 'success'
+
+@app.route('/word/<id>/edit', methods=['POST'])
+def edit_word(id):
+    word_id = id
+    req = request.get_json()
+    word = req['word']
+    meaning = req['meaning']
+    if word == '' or meaning == '':
+        flash('Please fill out all fields!', 'flash-error')
+    else:
+        conn = mysql.get_db()
+        cursor = conn.cursor()
+        cursor.execute('update word set word=%s, meaning=%s where id=%s', (word, meaning, word_id))
+        conn.commit()
+        cursor.close()
+        flash('Word succesffuly updated!', 'flash-success')
 
     return json.dumps('success')
 
--- static/scripts.js
+++ static/scripts.js
@@ -1,15 +1,18 @@
 window.onload = function() {
+    $('#modal').modal('show')
 
-    $('#word-index, #add-cancel').click(function() {
-        $('#word-add').removeClass('side-active')
+    $('#word-index, #add-cancel, #logo-cancel').click(function() {
+        $('#word-add, #logo-add').removeClass('side-active')
         $('#word-index').addClass('side-active')
-        $('#word-add-form').hide()
+        $('#word-add-form, #logo-add-form').hide()
     })
 
+    //add operation
     $('#word-add').click(function() {
-        $('#word-index').removeClass('side-active')
+        $('#word-index, #logo-add').removeClass('side-active')
         $(this).addClass('side-active')
         $('#word-add-form').show()
+        $('#logo-add-form').hide()
     })
 
     $('#word-add-form').submit(function() {
@@ -30,6 +33,31 @@ window.onload = function() {
         })
     })
 
+    //logo add operation
+    $('#logo-add').click(function() {
+        $('#word-index, #word-add').removeClass('side-active')
+        $(this).addClass('side-active')
+        $('#logo-add-form').show()
+        $('#word-add-form').hide()
+    })
+
+    $('#logo-add-form').submit(function() {
+        let data = new FormData()
+        data.append('file', $('#logo')[0].files[0])
+
+        $.ajax({
+            url: '/add_logo',
+            type: 'POST',
+            enctype: 'multipart/form-data',
+            data: data,
+            processData: false,
+            contentType: false,
+            success: () => location.reload(),
+            error: (err) => console.log(err)
+        })
+    })
+
+    //delete operation
     $('.delete').click(function() {
         let word_id = $(this).attr('id')
 
@@ -38,6 +66,49 @@ window.onload = function() {
             type: 'POST',
             success: () => { location.reload() },
             error:(err) => { console.log(err) }
+        })
+    })
+
+    //edit operation
+    var word, meaning
+
+    $('.edit').click(function() {
+        let parent = $(this).parents('tr')
+        parent.find('.edit-word, .edit-meaning').show()
+        parent.find('.dict-word, .dict-meaning').hide()
+        parent.find('.td-submit, .td-cancel').show()
+        parent.find('.edit, .delete').parent().hide()
+        word = parent.find('input').val()
+        meaning = parent.find('textarea').val()
+    })
+
+    $('.cancel').click(function() {
+        let parent = $(this).parents('tr')
+        parent.find('.edit-word, .edit-meaning').hide()
+        parent.find('.dict-word, .dict-meaning').show()
+        parent.find('.td-submit, .td-cancel').hide()
+        parent.find('.edit, .delete').parent().show()
+        parent.find('input').val(word)
+        parent.find('textarea').val(meaning)
+    })
+
+    $('.update-form').submit(function() {
+        let parent = $(this).parents('tr')
+        let newWord = parent.find('input').val()
+        let newMeaning = parent.find('textarea').val()
+        let word_id = parent.find('.submit').attr('id')
+
+        $.ajax({
+            url: '/word/' + word_id + '/edit',
+            type: 'POST',
+            dataType: 'json',
+            data: JSON.stringify({
+                'word': newWord,
+                'meaning': newMeaning
+            }),
+            contentType: 'application/json',
+            success: () => { location.reload() },
+            error: (err) => { console.log(err) }
         })
     })
 
